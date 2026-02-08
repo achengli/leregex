1febbeba77a3181c888c1879088a1c0152e0fcd7
achengli <tao3000ac@protonmail.com>
2026-02-09 00:01:11 +0100

    rockspec adapted
    Adapting format file for rockspec luarocks definitions

--- scripts/extract_version_from_rockspec.sh
+++ scripts/extract_version_from_rockspec.sh
@@ -4,19 +4,19 @@
 # Copyright (c) Yassn Achengli
 
 rockspec_file=$(ls $(dirname $0)/.. | grep -i leregex | xargs)
-version=$(cat $(dirname $0)/../$rockspec_file | grep -i version | cut -d= -f2 | grep -oE "[0-9]\.[0-9]\.[0-9]")
+version=$(cat $(dirname $0)/../$rockspec_file | grep -i version | cut -d= -f2 | grep -oE "[0-9]\.[0-9]-[0-9]")
 
 version_to_number(){
     local major=$(echo $1 | cut -d'.' -f1)
-    local minor=$(echo $1 | cut -d'.' -f2 | cut -d'.' -f1)
-    local release=$(echo $1 | cut -d'.' -f3)
+    local minor=$(echo $1 | cut -d'.' -f2 | cut -d'-' -f1)
+    local release=$(echo $1 | cut -d'-' -f2)
     echo $(( $release + $minor * 100 + $major * 10000 ))
 }
 
 version_make_flags(){
     local major=$(echo $1 | cut -d'.' -f1)
-    local minor=$(echo $1 | cut -d'.' -f2 | cut -d'.' -f1)
-    local release=$(echo $1 | cut -d'.' -f3)
+    local minor=$(echo $1 | cut -d'.' -f2 | cut -d'-' -f1)
+    local release=$(echo $1 | cut -d'-' -f2)
     echo "-MAJOR=${major} -MINOR=${minor} -RELEASE=${release}"
 }
 
